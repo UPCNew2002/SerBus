8d49a477d05f7e1d2410d66cf0afee9f9d68d78c
Hugo Tejeda <hugotejeda5286763@gmail.com>
2026-01-25 22:39:35 -0500

    mensaje

--- src/hooks/useColores.js
+++ src/hooks/useColores.js
@@ -4,17 +4,11 @@
 import { useEffect, useState } from 'react';
 import useTemaStore from '../store/temaStore';
 
+import useTemaStore from '../store/temaStore';
+ 
 export const useColores = () => {
+  // El selector de Zustand ya es reactivo, no necesitamos subscribe manual
   const colores = useTemaStore((state) => state.colores);
-  const [key, setKey] = useState(0);
-
-  // Forzar re-render cuando cambien los colores
-  useEffect(() => {
-    const unsubscribe = useTemaStore.subscribe(() => {
-      setKey((k) => k + 1);
-    });
-    return unsubscribe;
-  }, []);
 
   return colores;
 };
